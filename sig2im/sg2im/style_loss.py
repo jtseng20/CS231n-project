9543606487a64aba92fc1551626d5b270041a769
Helena Vasconcelos <helenavascc@gmail.com>
2021-05-14 18:57:29 -0700

    style loss

---
+++ sig2im/sg2im/style_loss.py
@@ -0,0 +1,124 @@
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import torchvision.transforms as transforms
+import torchvision.models as models
+
+import copy
+
+def gram_matrix(input):
+    a, b, c, d = input.size()  # a=batch size(=1)
+    # b=number of feature maps
+    # (c,d)=dimensions of a f. map (N=c*d)
+
+    features = input.view(a * b, c * d)  # resise F_XL into \hat F_XL
+
+    G = torch.mm(features, features.t())  # compute the gram product
+
+    # we 'normalize' the values of the gram matrix
+    # by dividing by the number of element in each feature maps.
+    return G.div(a * b * c * d)
+
+class StyleLoss(nn.Module):
+
+    def __init__(self):
+        super(StyleLoss, self).__init__()
+
+    def forward(self, input):
+        self.gram = gram_matrix(input)
+        return input
+
+
+# create a module to normalize input image so we can easily put it in a
+# nn.Sequential
+class Normalization(nn.Module):
+    def __init__(self, mean, std):
+        super(Normalization, self).__init__()
+        # .view the mean and std to make them [C x 1 x 1] so that they can
+        # directly work with image Tensor of shape [B x C x H x W].
+        # B is batch size. C is number of channels. H is height and W is width.
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
+
+    def forward(self, img):
+        # normalize img
+        return (img - self.mean) / self.std
+
+
+def get_style_model_and_losses(cnn, normalization_mean, normalization_std, 
+                               style_layers=['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']):
+    
+    cnn.eval()
+
+    # normalization module
+    normalization = Normalization(normalization_mean, normalization_std).to(device)
+
+    # just in order to have an iterable access to or list of content/syle
+    # losses
+    style_losses = []
+
+    # assuming that cnn is a nn.Sequential, so we make a new nn.Sequential
+    # to put in modules that are supposed to be activated sequentially
+    model = nn.Sequential(normalization)
+
+    i = 0  # increment every time we see a conv
+    for layer in cnn.children():
+        if isinstance(layer, nn.Conv2d):
+            i += 1
+            name = 'conv_{}'.format(i)
+        elif isinstance(layer, nn.ReLU):
+            name = 'relu_{}'.format(i)
+            # The in-place version doesn't play very nicely with the ContentLoss
+            # and StyleLoss we insert below. So we replace with out-of-place
+            # ones here.
+            layer = nn.ReLU(inplace=False)
+        elif isinstance(layer, nn.MaxPool2d):
+            name = 'pool_{}'.format(i)
+        elif isinstance(layer, nn.BatchNorm2d):
+            name = 'bn_{}'.format(i)
+        else:
+            raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
+
+        model.add_module(name, layer)
+
+        if name in style_layers:
+            # add style loss:
+            style_loss = StyleLoss()
+            model.add_module("style_loss_{}".format(i), style_loss)
+            style_losses.append(style_loss)
+            style_layers.pop(name)
+        if not style_layers:
+            break
+            
+    return model, style_losses
+
+
+def get_input_optimizer(input_img):
+    # this line to show that input is a parameter that requires a gradient
+    optimizer = optim.LBFGS([input_img.requires_grad_()])
+    return optimizer
+
+class StyleLossModule():
+    def __init__(self, cnn, cnn_normalization_mean, cnn_normalization_std):
+        self.model, self.style_losses = get_style_model_and_losses(cnn, cnn_normalization_mean, cnn_normalization_std)
+        
+    def get_style_score(self, style_img, input_img, style_weight=1000000):
+        self.model(style_img)
+        style_grams = [sl.gram.detach() for sl in self.style_losses]
+        self.model(input_img)
+        input_grams = [sl.gram.clone() for sl in self.style_losses]
+        
+        style_score = 0
+        for i_gram, s_gram in zip(input_grams, style_grams):
+            style_score += F.mse_loss(i_gram, s_gram)
+        style_score *= style_weight
+
+        return style_score
+    
